6db2f1ca90d36cdea8abe5608d30c8e2087ceb49
ggollo1 <35343408+ggollo1@users.noreply.github.com>
2018-02-26 23:45:42 -0600

    kn

--- js/codeassignment3.js
+++ js/codeassignment3.js
@@ -3,4 +3,4 @@ let desc= "gabrielle's portfolio"
 let projectTitle= "web app"
 let projectTitles = ["web app", "Book Assignment 1: Points of Interest, Waveland, MS "]
 let array= ["first", "second", "third", "fourth"]
-console.log("how many" + "array");
+console.log("how many" + "array);
