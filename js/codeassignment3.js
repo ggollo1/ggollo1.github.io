e6831bb4e8f7ec85785503d79ec96b1f1767460d
ggollo1 <35343408+ggollo1@users.noreply.github.com>
2018-02-28 19:28:51 -0600

    sds

--- js/codeassignment3.js
+++ js/codeassignment3.js
@@ -3,7 +3,7 @@ let desc= "gabrielle's portfolio"
 let bool= true
 let projectarray = ["web app", "Book Assignment 1: Points of Interest, Waveland, MS "]
 
-console.log('projectID');
-console.log('desc');
+console.log('ID: ' + projectID);
+console.log('Description: ' + desc);
 console.log('bool');
 console.log('projectarray');
