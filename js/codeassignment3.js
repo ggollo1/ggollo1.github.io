5ee358e5567aa08a2e3fc8a89f0ec801ea3cd8a2
ggollo1 <35343408+ggollo1@users.noreply.github.com>
2018-02-28 19:14:15 -0600

    did

--- js/codeassignment3.js
+++ js/codeassignment3.js
@@ -1,10 +1,9 @@
-let projects= 4
+let projectid= "1"
 let desc= "gabrielle's portfolio"
-let works= true
 let projectTitle= "web app"
 let projectarray = ["web app", "Book Assignment 1: Points of Interest, Waveland, MS "]
 
-console.log('how many projects' + projects;
-console.log('what is it' + desc);
-console.log(projectTitle);
-console.log(projectarray);
+console.log('projectID');
+console.log('desc');
+console.log('projectTitle');
+console.log('projectarray');
