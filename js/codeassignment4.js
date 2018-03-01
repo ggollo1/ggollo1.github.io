4b933b8da724441527211ba3ef686b44a0ad5966
ggollo1 <35343408+ggollo1@users.noreply.github.com>
2018-02-28 19:51:22 -0600

    sds

---
+++ js/codeassignment4.js
@@ -0,0 +1,27 @@
+let projectObject = {
+  id: 1,
+  url: 'https://ggollo1.github.io/',
+  title: 'My portfolio',
+  desc: 'Gabrielle's Portfolio',
+  bool: true,
+  keywords: [
+  'map', 
+  'tour', 
+  'coast', 
+  'beach'
+]
+}
+ let projectArray = [
+   1,
+   'https://ggollo1.github.io/',
+   'My portfolio',
+   'Gabrielle's Portfolio',
+   true,
+   ['map', 'tour', 'coast', 'beach']
+ ]
+
+
+
+ console.log(projectArray[2])
+ console.log(projectObject.desc)
+ console.log(projectObject["desc"])
