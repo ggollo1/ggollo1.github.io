5c9d76b1b1f8376a88bb76140a907d63433a3aa6
ggollo1 <35343408+ggollo1@users.noreply.github.com>
2018-03-22 11:51:42 -0500

    ffg

--- map/webmap-assignment-01.js
+++ map/webmap-assignment-01.js
@@ -1,1 +1,17 @@
 let tmap = L.map('abbysmap').setView([30.323136,	-89.420824], 13)
+let base = 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
+L.tileLayer(base).addTo(tmap)
+let mark = L.marker([30.266954	-89.388052]).addTo(tmap)
+let poly = L.polygon([
+  [30.259313,	-89.398349],
+  [30.321865,	-89.395774],
+  [30.305564,	-89.444869]
+]).addTo(tmap);
+
+poly.bindPopup('Waveland')
+mark.bindPopup('Home')
+
+function logCurrentCoordinates (event) {
+  console.log('You clicked the map at ' + event.latlng)
+}
+tmap.on('click', logCurrentCoordinates)
